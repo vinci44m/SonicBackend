272c3383ac4d27b6aef687d650926239825aac64
vinci44m <endresm32@gmail.com>
2026-07-02 00:02:12 +0200

    4.0

--- SonicBackend/routes/api.php
+++ SonicBackend/routes/api.php
@@ -1,34 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VideoController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\StreamController;
-use App\Http\Controllers\CommentController;
 
-// --- Authentifizierung (Öffentlich) ---
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// --- Öffentlich abrufbar (Auch ohne Login sichtbar) ---
 Route::get('/videos', [VideoController::class, 'index']);
+Route::get('/videos/{id}/comments', [VideoController::class, 'getComments']);
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/streams', [StreamController::class, 'index']);
-Route::get('/comments', [CommentController::class, 'index']); // Kommentare lesen darf jeder
 
-// --- Geschützte Routen (Nur für eingeloggte Nutzer via Sanctum) ---
 Route::middleware('auth:sanctum')->group(function () {
-    
-    // Benutzerprofil & eigene Uploads für "Mein Account"
-    Route::get('/user/me', [AuthController::class, 'me']);
-    
-    // Neue Inhalte erstellen
+    Route::get('/me', [AuthController::class, 'me']);
     Route::post('/videos', [VideoController::class, 'store']);
-    Route::post('/posts', [PostController::class, 'store']); // Neue Diskussionen im Backend speichern
-    
-    // Neuen Kommentar schreiben (für Video oder Post)
-    Route::post('/comments', [CommentController::class, 'store']);
-    
+    Route::post('/videos/{id}/comments', [VideoController::class, 'addComment']);
+    Route::post('/posts', [PostController::class, 'store']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user/me', [AuthController::class, 'me']);
+    Route::get('/me', [AuthController::class, 'me']); // Diese Zeile hinzufügen
 });
